4acba815d19383ed4dc7706ba533f90220ce55aa
sja31dev <sja31dev+bitbucket@gmail.com>
2017-11-06 11:02:12 +0000

    More finctionality

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -50,23 +50,65 @@ const allPolls = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allPolls: allPolls,
+      myPolls: [],
+      selectedPoll: null,
+      newPollInput: false
+    }
+  }
+
+  // COnvert this to a component ?
+  pollDisplay = (() => {
+    if (this.state.selectedPoll) {
+      return (
+        <div>
+          <h3 className="sect-title">Poll</h3>
+          <Poll poll={this.state.selectedPoll}/>
+        </div>
+      );
+    } else if (this.state.newPollInput) {
+      return (
+        <div>
+          <h3 className="sect-title">New Poll</h3>
+          <NewPoll />
+        </div>
+      );
+    } else {
+      return (
+        <div></div>
+      );
+    }
+  });
 
   render() {
     return (
       <div className="App container">
         <h1 className="text-center">Voting App</h1>
 
-        <h3 className="sect-title">New Poll</h3>
-        <NewPoll />
+        <div className="text-center">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => this.setState({selectedPoll: null, newPollInput: true})}>
+            Add Poll
+          </button>
+          <button type="button" className="btn btn-primary">Log in</button> / welcome user
+        </div>
 
-        <h3 className="sect-title">Poll</h3>
-        <Poll poll={allPolls[0]}/>
-
-        <h3 className="sect-title">My Polls</h3>
-        <PollList polls={allPolls} key="myPolls" />
+        {this.pollDisplay()}
 
         <h3 className="sect-title">All Polls</h3>
-        <PollList polls={allPolls} key = "allPolls" />
+        <PollList
+          polls={this.state.allPolls}
+          onPollSelect={selectedPoll => this.setState({selectedPoll: selectedPoll, newPollInput: false})}
+          key="allPolls" />
+
+        <h3 className="sect-title">My Polls</h3>
+        <PollList polls={this.state.myPolls} key = "myPolls" />
 
         <Footer />
       </div>

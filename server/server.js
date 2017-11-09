76c8adc09cb546b9993306d84532c8b5c8937777
Stephen Armstrong <29572908+sja31dev@users.noreply.github.com>
2017-11-09 01:23:28 +0000

    More routing adjustment

--- server/server.js
+++ server/server.js
@@ -247,12 +247,12 @@ app.get('/test', function(req, res) {
 // END OF USER AAUTHENTICATION
 
 // Static files
-app.use('*', express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use('/*', express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Remaining requests return the React app
-app.get('*', function(req, res) {
+/*app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-});
+});*/
 /*app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
 });*/
